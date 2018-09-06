1e4824cad6d3a2adc58334a28c7cd0449d023566
Levitina Veronika <levitinava@gmail.com>
2018-09-06 12:43:59 +0300

    переписана функция рандома убраны "магические числа"

--- js/pictures.js
+++ js/pictures.js
@@ -1,29 +1,47 @@
 'use strict';
 var photos = [];
-var comments = ['Всё отлично!',
+var comments = [
+  'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-var descriptions = ['Тестим новую камеру!',
+var descriptions = [
+  'Тестим новую камеру!',
   'Затусили с друзьями на море',
   'Как же круто тут кормят',
   'Отдыхаем...',
   'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......',
   'Вот это тачка!'];
+var MIN_LIKES = 15;
+var MAX_LIKES = 200;
+var COMMENTS_LENGTH = 10;
+var PHOTOS_LENGTH = 25;
+var CURRENT_PHOTO = 0;
 // Создайте массив, состоящий из 25 сгенерированных JS объектов, которые будут описывать фотографии, размещённые другими пользователями:
-var getRandomNumber = function (max, min) {
+var getRandomNumber = function (max, min, isFor) {
   if (typeof (min) === 'undefined') {
     min = 0;
   }
-  return Math.floor(Math.random() * (max - min) + min);
+  if (typeof (min) === 'boolean') {
+    isFor = min;
+    min = 0;
+  }
+  if (typeof (isFor) === 'undefined') {
+    isFor = true;
+  }
+  if (isFor) {
+    return Math.floor(Math.random() * (max - min) + min);
+  } else {
+    return Math.ceil(Math.random() * (max - min) + min);
+  }
 };
 var getUrl = function (i) {
-  return 'photos/' + (i + 1) + '.jpg';
+  return 'photos/' + i + '.jpg';
 };
 var getLikes = function () {
-  return getRandomNumber(201, 15);
+  return getRandomNumber(MAX_LIKES, MIN_LIKES, false);
 };
 var getNewComment = function () {
   return comments[getRandomNumber(comments.length)];
@@ -31,7 +49,7 @@ var getNewComment = function () {
 
 var getComments = function () {
   var arrayOfComments = [];
-  var arrayOfCommentsLength = getRandomNumber(10);
+  var arrayOfCommentsLength = getRandomNumber(COMMENTS_LENGTH);
   for (var i = 0; i < arrayOfCommentsLength; i++) {
     arrayOfComments[i] = getNewComment();
     if (getRandomNumber(2)) {
@@ -45,9 +63,9 @@ var getDescription = function () {
   return descriptions[getRandomNumber(descriptions.length)];
 };
 
-for (var i = 0; i < 25; i++) {
+for (var i = 0; i < PHOTOS_LENGTH; i++) {
   photos[i] = {};
-  photos[i].url = getUrl(i);
+  photos[i].url = getUrl(i + 1);
   photos[i].likes = getLikes();
   photos[i].comments = getComments();
   photos[i].description = getDescription();
@@ -78,6 +96,7 @@ var drawPictures = function () {
   pictures.appendChild(fragment);
 };
 drawPictures();
+
 // Покажите элемент .big-picture, удалив у него класс .hidden и заполните его данными из первого элемента сгенерированного вами массива:
 var showElement = function (element) {
   element.classList.remove('hidden');
@@ -85,15 +104,16 @@ var showElement = function (element) {
 
 var bigPicture = document.querySelector('.big-picture');
 showElement(bigPicture);
+
 // url avatar
 var getAvatarUrl = function () {
-  return 'img/avatar-' + getRandomNumber(7, 1) + '.svg';
+  return 'img/avatar-' + getRandomNumber(6, false) + '.svg';
 };
 var createBigPicture = function () {
-  bigPicture.querySelector('.big-picture__img').setAttribute('src', photos[1].url);
-  bigPicture.querySelector('.likes-count').textContent = photos[1].likes;
-  bigPicture.querySelector('.comments-count').textContent = photos[1].comments.length;
-  bigPicture.querySelector('.social__caption').textContent = photos[1].description;
+  bigPicture.querySelector('.big-picture__img').querySelector('img').setAttribute('src', photos[CURRENT_PHOTO].url);
+  bigPicture.querySelector('.likes-count').textContent = photos[CURRENT_PHOTO].likes;
+  bigPicture.querySelector('.comments-count').textContent = photos[CURRENT_PHOTO].comments.length;
+  bigPicture.querySelector('.social__caption').textContent = photos[CURRENT_PHOTO].description;
 
   var socialComments = bigPicture.querySelector('.social__comments');
   var commentFragment = document.createDocumentFragment();
@@ -107,17 +127,18 @@ var createBigPicture = function () {
   }
 
   var addCommentFragment = function (numberOfComment) {
-    commentTemplate.querySelector('p').textContent = photos[1].comments[numberOfComment];
+    commentTemplate.querySelector('p').textContent = photos[CURRENT_PHOTO].comments[numberOfComment];
     commentTemplate.querySelector('img').setAttribute('src', getAvatarUrl());
     return commentTemplate.cloneNode(true);
   };
-  for (i = 0; i < photos[1].comments.length; i++) {
+  for (i = 0; i < photos[CURRENT_PHOTO].comments.length; i++) {
     commentFragment.appendChild(addCommentFragment(i));
   }
   socialComments.appendChild(commentFragment);
 };
 
 createBigPicture();
+
 // Спрячьте блоки счётчика комментариев .social__comment-count и загрузки новых комментариев .comments-loader,
 // добавив им класс .visually-hidden.
 var socialCommentCount = document.querySelector('.social__comment-count');
@@ -125,5 +146,6 @@ var commentsLoader = document.querySelector('.comments-loader');
 var hideElements = function (element) {
   element.classList.add('visually-hidden');
 };
+
 hideElements(socialCommentCount);
 hideElements(commentsLoader);
