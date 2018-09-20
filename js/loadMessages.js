7629ce5195ca2c3570647600c0cdf276dad40078
Levitina Veronika <levitinava@gmail.com>
2018-09-20 16:20:07 +0300

    закрытие сообщений об ошибках

--- js/loadMessages.js
+++ js/loadMessages.js
@@ -21,6 +21,10 @@
     .querySelector('.error');
   var errorMessage = loadErrorMessageTemplate.cloneNode(true);
 
+  var onOverlayMessageClick = function () {
+
+  };
+
 
   window.loadMessages = {
 
@@ -32,9 +36,30 @@
     },
     onLoadSuccessMessage: function () {
       window.loadMessages.addMessage(successMessage);
+      var successInner = document.querySelector('.success__inner');
+      var onSuccessMessageClick = function (evt) {
+        if (evt.target !== successInner) {
+          errorMessage.removeEventListener('click', onSuccessMessageClick);
+          window.loadMessages.deleteSuccessMessage();
+          window.form.imgUploadForm.reset();
+        }
+
+      };
+      var onDocumentPressEsc = function (evt) {
+        if (evt.keyCode === window.util.ESC_KEYCODE) {
+          window.loadMessages.deleteSuccessMessage();
+          window.form.imgUploadForm.reset();
+          document.removeEventListener('keyup', onDocumentPressEsc);
+        }
+      };
+      document.addEventListener('keyup', onDocumentPressEsc);
+
+      successMessage.addEventListener('click', onSuccessMessageClick);
+
       var button = successMessage.querySelector('button');
       var onButtonClick = function () {
         window.loadMessages.deleteSuccessMessage();
+        window.form.imgUploadForm.reset();
         button.removeEventListener('click', onButtonClick);
       };
       button.addEventListener('click', onButtonClick);
@@ -42,8 +67,28 @@
     deleteSuccessMessage: function () {
       this.deleteMessage(successMessage);
     },
+
     onLoadErrorMessage: function () {
       window.loadMessages.addMessage(errorMessage);
+      var errorInner = document.querySelector('.error__inner');
+      var onErrorMessageClick = function (evt) {
+        if (evt.target !== errorInner) {
+          errorMessage.removeEventListener('click', onErrorMessageClick);
+          window.loadMessages.deleteErrorMessage();
+          window.form.imgUploadForm.reset();
+        }
+      };
+      errorMessage.addEventListener('click', onErrorMessageClick);
+
+      var onDocumentPressEsc = function (evt) {
+        if (evt.keyCode === window.util.ESC_KEYCODE) {
+          window.loadMessages.deleteErrorMessage();
+          window.form.imgUploadForm.reset();
+          document.removeEventListener('keyup', onDocumentPressEsc);
+        }
+      };
+      document.addEventListener('keyup', onDocumentPressEsc);
+
       var errorButtonTryAgain = errorMessage.querySelector('button:first-child');
       var errorButtonApplyAnotherFile = errorMessage.querySelector('button:last-child');
       var onButtonTryAgainClick = function () {
